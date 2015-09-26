861773d5f232bcdf26f0178baa902ac3e487e408
Vincent Daubry <vdaubry@gmail.com>
2015-09-26 09:48:57 +0200

    add sample model + monitoring service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def monitoring
+    db_status = User.count >= 0
+    render json: {database: db_status }
+  end
 end
